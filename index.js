b5cd95246cb4877082423c1ece48ce224985b1b8
Breno Brito <breno.silvabrito@hotmail.com>
2022-07-17 18:43:08 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// npm install bcrypt express cookie-parser cors jsonwebtoken mongoose nodemon multer
+
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+// Config JSON response
+app.use(express.json())
+
+//In this project we do not need URLenconded as we are only going to work with json 
+
+// Solve CORS
+app.use(cors({ credentials: true, origin: 'http://localhost:3000'}))
+
+// Public fold for images
+app.use(express.static('public'))
+
+// Routes
+
+app.listen(5000)
