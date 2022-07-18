293ff50b2b691e1f9ca7cf4f05559e17adcd22be
Breno Brito <breno.silvabrito@hotmail.com>
2022-07-18 18:28:23 +0100

    Create new User, adding it to the database with encrypted password

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
 module.exports = class UserController {
 
@@ -44,6 +45,29 @@ module.exports = class UserController {
             res.status(422).json({message: 'Este email ja esta cadastrado'})
             return
         }
+
+        //create a password
+        const salt = await bcrypt.genSalt(12)
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        // create a user 
+        const user = new User({
+            name,
+            email,
+            phone,
+            password: passwordHash,
+        })
+
+        try {
+            
+            const newUser = await user.save()
+            res.status(201).json({message: 'Usuario criado!', newUser})
+            return
+
+        } catch (error) {
+            res.status(500).json({message: error})
+        }
     }
+
 
 }
