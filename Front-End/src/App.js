381e7d4a2590fb4f8413ad8e130799af2d2eb7e1
Breno Brito <breno.silvabrito@hotmail.com>
2022-07-25 17:20:10 +0100

    Insert react router to the project

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+/* pages */
+import Login from './components/pages/Auth/Login'
+import Register from './components/pages/Auth/Register'
+import Home from './components/pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Get A Pet</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
