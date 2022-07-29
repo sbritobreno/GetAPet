31678d040ea67df610b0644eaf47c5b14ba03982
Breno Brito <breno.silvabrito@hotmail.com>
2022-07-29 15:07:18 +0100

    Update user picture

--- Front-End/src/components/pages/User/Profile.js
+++ Front-End/src/components/pages/User/Profile.js
@@ -7,6 +7,7 @@ import useFlashMessage from '../../../hooks/useFlashMEssage'
 
 function Profile() {
   const [user, setUser] = useState({})
+  const [preview, setPreview] = useState('')
   const [token] = useState(localStorage.getItem('token') || '')
   const {setFlashMessage} = useFlashMessage()
 
@@ -23,6 +24,7 @@ function Profile() {
   }, [token])
 
   function onFileChange(e){
+    setPreview(e.target.files[0])
     setUser({...user, [e.target.name]: e.target.files[0]})
   }
 
@@ -58,7 +60,12 @@ function Profile() {
       <section>
           <div className={styles.profile_header}>
             <h1>Perfil</h1>
-            <p>Preview imagem</p>
+            {(user.image || preview) && (
+              <img 
+                src={preview ? URL.createObjectURL(preview) : `${process.env.REACT_APP_API}/images/users/${user.image}`}
+                alt={user.name}
+              />
+            )}
           </div>
           <form onSubmit={handleSubmit} className={formStyles.form_container}>
             <Input 
